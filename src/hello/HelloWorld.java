07e07b6c0d0a9af3307ffedef9ca0b537b1ebc3a
84538 <84538@192.168.120.1>
2020-02-18 18:18:30 +0800

    test

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,36 @@
+package hello;
+
+import java.util.ArrayList;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 50;
+		int[]primes = new int[num];
+		primes[0]=2;
+		int cnt = 1;
+		MAXLOOP:
+		for(int x=3;cnt<50;x++)
+		{
+			for(int i=0;i<cnt;i++)
+			{
+				if(x%primes[i]==0)
+					continue MAXLOOP;
+			}
+			primes[cnt++] = x;
+		}
+		for(int k:primes)
+		{
+			System.out.println(k+" ");
+		}
+//		ArrayList<String>a = new ArrayList<String>();
+//		a.add("first");
+//		a.add("second");
+//		for(String s:a)
+//		{
+//			System.out.println(s);
+//		}
+	}
+
+}
